2dd2536bf9689b0e258a86d2f449b7d9b8bf0bc3
Srinivasan-Akash <prashanthiakash@gmail.com>
2023-05-20 23:55:06 +0530

    Added mini tools section

--- static/index.js
+++ static/index.js
@@ -4,6 +4,8 @@ const SIGN_OUT_BUTTON = document.querySelector(".log-out");
 const MOTIVATIONAL_QUOTE = document.querySelector(".quote");
 const MOTIVATONAL_QUOTE_FOUNDER = document.querySelector(".quote-founder");
 const GO_BACK_FROM_WEBSITE_INFO = document.querySelector(".goBack-websiteInfo");
+const GO_BACK_FROM_MINI_TOOLS = document.querySelector(".goBack-miniTools");
+const MINI_TOOLS_BUTTON = document.querySelector(".tools");
 
 // Email Form elements
 const EMAIL_SUBMIT_BUTTON = document.querySelector(".share-feature button");
@@ -16,12 +18,14 @@ const GET_WEBSITE_INFO_BUTTON = document.querySelector(".util.websiteInfo");
 const SIGN_IN_SCREEN = document.querySelector(".sign-in");
 const DASHBOARD_SCREEN = document.querySelector(".dashboard");
 const WEBSITE_INFO = document.querySelector(".WebsiteInfo");
+const MINI_TOOLS = document.querySelector(".mini-tools");
 
 // Props
 const RED_NEON_BLOB = document.querySelector(".red");
 const BLUE_NEON_BLOB = document.querySelector(".blue");
 
 // Event Listeners
+// Form & To Dashboard & Login
 SIGN_IN_BUTTON.addEventListener("click", () => {
   RED_NEON_BLOB.style.left = "50%";
   BLUE_NEON_BLOB.style.right = "50%";
@@ -36,6 +40,7 @@ SIGN_OUT_BUTTON.addEventListener("click", () => {
   DASHBOARD_SCREEN.style.scale = "0";
 });
 
+// MAIL
 EMAIL_SUBMIT_BUTTON.addEventListener('click', () => {
   vscode.window.showInformationMessage('Message Sent Sucessfully !!');
 
@@ -57,6 +62,7 @@ EMAIL_SUBMIT_BUTTON.addEventListener('click', () => {
     });
 });
 
+// From & To (GetWebsiteInfo To Dashboard)
 GET_WEBSITE_INFO_BUTTON.addEventListener('click', () => {
   RED_NEON_BLOB.style.left = "0%";
   BLUE_NEON_BLOB.style.right = "0%";
@@ -69,7 +75,22 @@ GO_BACK_FROM_WEBSITE_INFO.addEventListener('click', () => {
   BLUE_NEON_BLOB.style.right = "50%";
   WEBSITE_INFO.style.scale = "0";
   DASHBOARD_SCREEN.style.scale = "1";
-})
+});
+
+// From & To (miniTools To Dashboard)
+MINI_TOOLS_BUTTON.addEventListener('click', () => {
+  RED_NEON_BLOB.style.left = "0%";
+  BLUE_NEON_BLOB.style.right = "0%";
+  DASHBOARD_SCREEN.style.scale = "0";
+  MINI_TOOLS.style.scale = "1";
+});
+
+GO_BACK_FROM_MINI_TOOLS.addEventListener('click', () => {
+  RED_NEON_BLOB.style.left = "50%";
+  BLUE_NEON_BLOB.style.right = "50%";
+  MINI_TOOLS.style.scale = "0";
+  DASHBOARD_SCREEN.style.scale = "1";
+});
 
 // Functionality
 getJoke();
